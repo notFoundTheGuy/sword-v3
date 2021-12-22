fb201f15dd8d04338f9073dfe851f349936eac46
lister <listerli@tencent.com>
2021-12-22 16:27:34 +0800

    fix: 代码优化

--- src/pages/lab/index.vue
+++ src/pages/lab/index.vue
@@ -1,47 +1,48 @@
 <template>
-	<section>
-		<div class="main-menu">
-			<a-menu mode="inline" style="width: 100%" @select="onMenuSelect">
-				<a-sub-menu v-for="(item, index) in menu">
-					<template #title>
-						{{ item.name }}
-					</template>
+  <section>
+    <div class="main-menu">
+      <a-menu mode="inline" style="width: 100%" @select="onMenuSelect">
+        <a-sub-menu v-for="(item, index) in menu" :key="index">
+          <template #title>
+            {{ item.name }}
+          </template>
 
-					<a-menu-item v-for="(subItem, subIndex) in item.children">
-						{{ subItem.name }}
-					</a-menu-item>
-				</a-sub-menu>
-			</a-menu>
-		</div>
+          <a-menu-item
+            v-for="(subItem, subIndex) in item.children"
+            :key="subIndex"
+          >
+            {{ subItem.name }}
+          </a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </div>
 
-		<div class="main-content">
-			陆续整理中。。。
-		</div>
-	</section>
+    <div class="main-content">陆续整理中。。。</div>
+  </section>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue';
-import LabList from './LabList';
+import { defineComponent, onMounted, ref } from "vue";
+import LabList from "./LabList";
 
 export default defineComponent({
-	name: 'Lab',
-	setup(props, context) {
-		let menu = ref();
+  name: "Lab",
+  setup(props, context) {
+    let menu = ref();
 
-		const onMenuSelect = (item: any) => {
-			console.log(item);
-		};
+    const onMenuSelect = (item: any) => {
+      console.log(item);
+    };
 
-		onMounted(() => {
-			menu.value = LabList;
-		});
+    onMounted(() => {
+      menu.value = LabList;
+    });
 
-		return {
-			menu,
-			onMenuSelect,
-		};
-	},
+    return {
+      menu,
+      onMenuSelect,
+    };
+  },
 });
 </script>
 
